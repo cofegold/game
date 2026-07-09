6f5dd6b71e242d43f55d5da889de945a4e4896a9
cofegold <cofegold2025@gmail.com>
2026-07-09 05:38:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print('Telegram AI Game template')
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("سلام 👋")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
